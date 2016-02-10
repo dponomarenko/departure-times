96b36d6fbacef7d775707566d0d54ccce499b43a
Danila Ponomarenko <Danila.Ponomarenko@corp.mail.ru>
2016-02-11 01:16:02 +0300

    Procfile&Runner created for Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JAVA_OPTS -cp target/classes:target/dependency/* com.uber.departure.times.HerokuRunner -DLOG_PATH=/var/log/
---
+++ src/main/java/com/uber/departure/times/HerokuRunner.java
@@ -0,0 +1,30 @@
+package com.uber.departure.times;
+
+import com.uber.departure.times.crawler.nextbus.NextBusCrawlerVerticle;
+import com.uber.departure.times.hub.server.HubVerticle;
+import com.uber.departure.times.rest.RestVerticle;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+
+/**
+ * @author Danila Ponomarenko
+ */
+public final class HerokuRunner {
+    private static final Logger logger = LoggerFactory.getLogger(HerokuRunner.class);
+
+    public static void main(String[] args) {
+        Vertx.clusteredVertx(new VertxOptions(), r -> {
+            if (r.succeeded()) {
+                final Vertx vertx = r.result();
+                vertx.deployVerticle(new HubVerticle());
+                vertx.deployVerticle(new RestVerticle());
+                vertx.deployVerticle(new NextBusCrawlerVerticle());
+            } else {
+                logger.error("Failed to start vertx", r.cause());
+            }
+        });
+    }
+}
